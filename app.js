44547b6171999f7f2a8a8aca6fb49e4d7c89c123
Turqut <turqutismayil007@gmail.com>
2026-07-03 23:20:35 +0400

    fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const JWT_SECRET = "menimSecretKeyim";
 
 app.use(
   cors({
-    origin: "https://jwt-front-j79w.vercel.app/",
+    origin: "https://jwt-front-j79w-43g56o9mb-turgut707.vercel.app/",
   }),
 );
 app.use(express.json());
